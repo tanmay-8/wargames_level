eb28e7396f5fa5158606d0b80c078dad238f2de8
tanmay-8 <tanmayshingde8@gmail.com>
2024-07-15 20:29:41 +0530

    first

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Code is not here , you are at wrong place ! But work is half done , keep going !")
+app.get("/", (req, res) => {
+    if (req.headers["user-agent"].includes("curl")) {
+        res.send(
+            "<head><link flag='go to api/34334'><title>War Games</title></head><h1>Code is not here but work is half done !</h1>"
+        );
+    } else {
+        res.send(
+            "<head><link flag='Don't be oversmart'><title>War Games</title></head><h1>You are good in browsing but not in linux!</h1>"
+        );
+    }
 });
 
+app.get("/api/34334", (req, res) => {
+    if (req.headers["user-agent"].includes("curl")) {
+        res.send(
+            "<head><link flag='You are smart but you cannot GET me'><title>War Games</title></head><h1>You ar near, show some inspecting skills</h1>"
+        );
+    } else {
+        res.send(
+            "<head><link flag='You are just wandering'><title>War Games</title></head><h1>You are just wandering</h1>"
+        );
+    }
+});
+
+app.post("/api/34334", (req, res) => {
+    if (req.headers["user-agent"].includes("curl")) {
+        res.send("FLAG : WLUG{TANS8759}");
+    }else{
+        res.send("Expected better from you");
+    }
+});
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000")
-})
-
-
+    console.log("Server is running on port 5000");
+});
